0bbb184ef85385915c40d9da8c0a1bcc351fc1d3
Divyansh <divyanshracvik2209@gmail.com>
2024-01-06 18:28:27 +0530

    size change

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ const Skills = () => {
   return (
     <section id="skills" className="section">
       <div className="skills__container">
-        <CircularProgressBar skillName="HTML" percentage="75" circleWidth="150" />
+        <CircularProgressBar skillName="HTML" percentage="75" circleWidth="200" />
         <CircularProgressBar skillName="CSS" percentage="80" circleWidth="200" />
         <CircularProgressBar skillName="Bootstrap" percentage="65" circleWidth="200" />
         <CircularProgressBar skillName="Javacript" percentage="70" circleWidth="200" />
